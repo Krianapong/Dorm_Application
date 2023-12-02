0e935106728e829c6048c3ee126019c563bb20e2
Krisanapong Jareunjanporn <150851052+Krianapong@users.noreply.github.com>
2023-12-03 00:53:40 +0700

    up

--- src/components/home/recent/RecentCard.jsx
+++ src/components/home/recent/RecentCard.jsx
@@ -1,39 +1,82 @@
-import React from "react"
-import { list } from "../../../data/Data"
+import React, { useState, useEffect } from "react";
+import { firestore } from "../../../firebase"; // แทนที่ด้วย configuration ของ Firebase ที่คุณใช้
 
 const RecentCard = () => {
+  const [zones, setZones] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = [];
+
+        // ดึงข้อมูลโซนทั้งหมด
+        const zonesSnapshot = await firestore.collection("zones").get(); // แทนที่ด้วยชื่อคอลเลคชันที่คุณใช้
+
+        // วนลูปทุกรายการในโซน
+        for (const zoneDoc of zonesSnapshot.docs) {
+          // ดึงข้อมูลห้องภายในแต่ละโซน
+          const roomsSnapshot = await firestore
+            .collection("zones")
+            .doc(zoneDoc.id)
+            .collection("rooms")
+            .get();
+
+          // วนลูปทุกรายการห้องและเก็บข้อมูล
+          const zoneData = {
+            id: zoneDoc.id,
+            name: zoneDoc.data().name,
+            rooms: roomsSnapshot.docs.map((roomDoc) => roomDoc.data()),
+          };
+
+          data.push(zoneData);
+        }
+
+        
+
+        setZones(data);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div className='content grid3 mtop'>
-        {list.map((val, index) => {
-          const { cover, category, location, name, price, type } = val
-          return (
-            <div className='box shadow' key={index}>
-              <div className='img'>
-                <img src={cover} alt='' />
-              </div>
-              <div className='text'>
-                <div className='category flex'>
-                  <span style={{ background: category === "For Sale" ? "#25b5791a" : "#ff98001a", color: category === "For Sale" ? "#25b579" : "#ff9800" }}>{category}</span>
-                  <i className='fa fa-heart'></i>
-                </div>
-                <h4>{name}</h4>
-                <p>
-                  <i className='fa fa-location-dot'></i> {location}
-                </p>
-              </div>
-              <div className='button flex'>
-                <div>
-                  <button className='btn2'>{price}</button> <label htmlFor=''>/sqft</label>
-                </div>
-                <span>{type}</span>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    </>
-  )
-}
+      {zones.map((zone) => (
+        <div key={zone.id}>
+          <h2>{zone.name}</h2>
+          <div className='content grid3 mtop'>
+            {zone.rooms.map((room, index) => {
+              const { image, roomNumber, name, price, type } = room;
 
-export default RecentCard
+              return (
+                <div className='box shadow' key={index}>
+                  <div className='img'>
+                    <img src={image} alt='' />
+                  </div>
+                  <div className='text'>
+                    <h4>{roomNumber}</h4>
+                    <p>
+                      <i className='fa fa-location-dot'></i> {name}
+                    </p>
+                  </div>
+                  <div className='button flex'>
+                    <div>
+                      <button className='btn2'>{price}</button>{" "}
+                      <label htmlFor=''>/sqft</label>
+                    </div>
+                    <span>{type}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default RecentCard;
