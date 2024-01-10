6de31d07d1bbec95c756a2808803b9554059f309
suwannapoom yabua <120366697+suwannapoom5@users.noreply.github.com>
2024-01-10 20:31:34 +0700

    fix icon

--- src/data/Data.js
+++ src/data/Data.js
@@ -58,29 +58,29 @@ export const blog = [
 ]
 export const featured = [
   {
-    cover: "../images/hero/h1.png",
-    name: "Family House",
-    total: "122 Property",
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "5 Building ",
   },
   {
     cover: "../images/hero/h2.png",
-    name: "House & Villa",
-    total: "155 Property",
+    name: "Vacant room",
+    total: "35 room",
   },
   {
-    cover: "../images/hero/h3.png",
-    name: "Apartment",
-    total: "300 Property",
+    cover: "../images/hero/h1.png",
+    name: "Reserve a room",
+    total: "10  room",
   },
   {
-    cover: "../images/hero/h4.png",
-    name: "Office & Studio",
-    total: "80 Property",
+    cover: "../images/hero/bills.png",
+    name: "Water bill",
+    total: "10 Unit",
   },
   {
-    cover: "../images/hero/h6.png",
-    name: "Villa & Condo",
-    total: "80 Property",
+    cover: "../images/hero/bill.png",
+    name: "Electricity bill",
+    total: "80 Unit",
   },
 ];
 export const list = [
